8678d1f17fea3d47f1e5217d97c302904fdf8458
Michael Johansyah <johansyahmichael@gmail.com>
2025-03-13 22:52:00 +0700

    Make getComputerChoice function, functioning normally

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("tess");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+  randomValue = getRandomInt(3);
+  switch (randomValue) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    default:
+      return "Scissors";
+  }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
